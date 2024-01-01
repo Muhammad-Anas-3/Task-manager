b2a882f0af4be2ff047f0d50698ebcb1664803c5
Muhammad-Anas-3 <134198144+Muhammad-Anas-3@users.noreply.github.com>
2024-01-01 11:57:29 +0500

    updated

--- db/connectdb.js
+++ db/connectdb.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectDb = (url) => {
     return mongoose
         .connect(url)
-        .then(() => console.log("COnnected to mongo-Db..."))
+        .then(() => console.log("Connected to mongo-Db..."))
         .catch((err) => console.log(err, "Not connected"))
 }
 
