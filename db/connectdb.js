450fecad1c3c5821ca9b5ebade946cf2ceb5b9d5
Muhammad-Anas-3 <134198144+Muhammad-Anas-3@users.noreply.github.com>
2024-01-01 11:56:59 +0500

    updated

--- db/connectdb.js
+++ db/connectdb.js
@@ -1,9 +1,5 @@
 import mongoose from "mongoose";
 
-// console.log(process.env.MONGO_DB_URI)
-// console.log(process.env.PORT)
-const conec = 'mongodb+srv://muhammadanas:MAnas1234@nodeprojects-db.lsqfoxo.mongodb.net/Task-Manager'
-
 const connectDb = (url) => {
     return mongoose
         .connect(url)
